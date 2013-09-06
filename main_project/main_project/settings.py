7d11b22d681e6773452fca6685afa7b69ca21da0
Ricardo A Corredor <ra.corredor@gmail.com>
2013-09-06 16:39:20 +0200

    Succesful test using XTK! Displaying two VTK objects saved in ASCII format

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -61,7 +61,7 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH,'media_files')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_URL = 'http://134.214.236.122:80/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
